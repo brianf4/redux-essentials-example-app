0d8d04b4c66eb105753536457fcc9c2576155e9e
Brian Fuentes <Brian.Fuentes04@gmail.com>
2023-06-07 20:49:22 -0700

    completed redux fundamentals

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postsSlice'
 import userReducer from '../features/users/usersSlice'
+import notificationsReducer from '../features/notifications/notificationsSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
-    users: userReducer
+    users: userReducer,
+    notifications: notificationsReducer
   }
 })
